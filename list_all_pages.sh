3a76c56725454e4f64a43f616b3efb14000041d2
SoyaDokio <zdqdj@qq.com>
2021-08-23 01:51:33 +0800

    Create list_all_pages.sh
    用以列出文件夹 data 下的所有文件（不含文件夹、不递归）的名称，并保存进文件夹 data 下的 list.txt 文件，以供用户访问查看。

---
+++ list_all_pages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+  $(ls -l --time-style=full /app/data | grep ^[^d] | awk '{printf "%-11s%-9s　%s\n",$6,substr($7,1,8),$9}' > /app/data/list.txt)
+  sleep 1 #间隔的秒数，不能大于60
+done
